c8ace450ff5b05a005c9473072cca0df1c2f27d0
Christine Chen <chen.christine17@berkeley.edu>
2016-03-10 22:47:26 -0800

    proj2 progress

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet var hangmanImageView: UIImageView!
+    @IBOutlet var wordToGuessLabel: UILabel!
+    
+    var numberOfWrongGuesses = 0
+    var phraseArray = [Character]()
+    var correctGuessArray = [Character]()
+    var dashesAndSpaces = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
+        
+        hangmanImageView.image = UIImage(named: "hangman1.gif")
+        
+        for letter in phrase.characters {
+            phraseArray.append(letter)
+            if (letter == " ") {
+                dashesAndSpaces += " "
+            }
+            dashesAndSpaces += "-"
+        }
+        wordToGuessLabel.text = dashesAndSpaces
         print(phrase)
     }
 
@@ -24,7 +43,32 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func letterGuess(button: UIButton?) {
+        var counter = 0
+        while phraseArray.count > counter {
+            if String(phraseArray[counter]) == button!.titleLabel!.text {
+                correctGuessArray[counter] = phraseArray[counter]
+            }
+            counter += 1
+        }
+    }
+    
+    func hangmanImage() {
+        if numberOfWrongGuesses == 1 {
+            hangmanImageView.image = UIImage(named: "hangman2.gif")
+        } else if numberOfWrongGuesses == 2 {
+            hangmanImageView.image = UIImage(named: "hangman3.gif")
+        } else if numberOfWrongGuesses == 3 {
+            hangmanImageView.image = UIImage(named: "hangman4.gif")
+        } else if numberOfWrongGuesses == 4 {
+            hangmanImageView.image = UIImage(named: "hangman5.gif")
+        } else if numberOfWrongGuesses == 5 {
+            hangmanImageView.image = UIImage(named: "hangman6.gif")
+        } else {
+            hangmanImageView.image = UIImage(named: "hangman7.gif")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
